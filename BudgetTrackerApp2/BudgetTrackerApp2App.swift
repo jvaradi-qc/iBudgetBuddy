8ffbff223ce88e06bcc567eba07482f3a3890151
John Varadi <jvnycpsu1993@gmail.com>
2026-01-05 19:49:54 -0800

    Initial Commit

---
+++ BudgetTrackerApp2/BudgetTrackerApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp2App.swift
+//  BudgetTrackerApp2
+//
+//  Created by JOHN VARADI on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTrackerApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BudgetTrackerApp2
+//
+//  Created by JOHN VARADI on 1/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
